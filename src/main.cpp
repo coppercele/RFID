05487823b205c9f98b90e3c6b780742f3cfb9732
coppercele <mokeuhi@hotmail.com>
2022-11-16 19:12:44 +0900

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,160 @@
+
+#include <Arduino.h>
+#include "MFRC522_I2C.h"
+#include <M5Stack.h>
+#define LGFX_USE_V1
+#include <LovyanGFX.hpp>
+#define LGFX_AUTODETECT
+#include <LGFX_AUTODETECT.hpp>
+
+MFRC522 mfrc522(0x28); // Create MFRC522 instance.  创建MFRC522实例
+
+static LGFX lcd;
+static LGFX_Sprite sprite(&lcd);
+
+struct beans {
+  int day = 0;
+  int hour = 0;
+  int minute = 0;
+  int dateIndex = 2;
+  int mode = 0;
+  byte uidByte[10];
+  byte uidSize = 0;
+} data;
+
+void makeSprite() {
+
+  sprite.clear(TFT_BLACK);
+  sprite.setFont(&fonts::lgfxJapanGothicP_20);
+  sprite.setTextSize(1);
+  sprite.setTextColor(TFT_WHITE, TFT_BLACK);
+  sprite.setTextSize(2);
+  if (data.uidSize != 0) {
+    sprite.setCursor(0, 0);
+    sprite.printf("ID: ");
+    for (int i = 0; i < data.uidSize; i++) {
+      sprite.printf("%02X ", data.uidByte[i]);
+    }
+  }
+
+  sprite.setCursor(0, 150);
+  sprite.printf("  ");
+  for (int i = 0; i < data.dateIndex; i++) {
+    sprite.printf("　　 ");
+  }
+  sprite.setTextSize(3);
+  sprite.printf("%s", "■");
+
+  sprite.setTextSize(2);
+  sprite.setCursor(0, 190);
+  switch (data.mode) {
+  case 0:
+    sprite.printf("%s", "  登録　↑ 　←");
+    break;
+  case 1:
+    sprite.printf("%s", "  削除　↑ 　←");
+    break;
+
+  default:
+    break;
+  }
+
+  sprite.setCursor(0, 130);
+  sprite.printf("  %02d日%02d時%02d分", data.day, data.hour, data.minute);
+
+  sprite.pushSprite(&lcd, 0, 0);
+}
+
+void setup() {
+  M5.begin();       // Init M5Stack.  初始化M5Stack
+  M5.Power.begin(); // Init power  初始化电源模块
+  Wire.begin(); // Wire init, adding the I2C bus.  Wire初始化, 加入i2c总线
+
+  mfrc522.PCD_Init(); // Init MFRC522.  初始化 MFRC522
+                      // M5.Lcd.println("Please put the card\n\nUID:");
+  lcd.init();
+  lcd.setRotation(1);
+
+  lcd.setBrightness(200);
+
+  sprite.setPsram(true);
+  sprite.setColorDepth(8);
+  sprite.createSprite(320, 240);
+  makeSprite();
+}
+
+void loop() {
+  M5.update();
+  if (M5.BtnA.wasPressed()) {
+    switch (data.mode) {
+    case 0:
+      data.mode = 1;
+      break;
+    case 1:
+      data.mode = 0;
+      break;
+
+    default:
+      break;
+    }
+    makeSprite();
+  }
+
+  if (M5.BtnC.wasPressed()) {
+    if (data.dateIndex == 0) {
+      data.dateIndex = 2;
+    }
+    else {
+      data.dateIndex--;
+    }
+
+    makeSprite();
+  }
+  if (M5.BtnB.wasPressed()) {
+    switch (data.dateIndex) {
+    case 2:
+      if (59 <= data.minute) {
+        data.minute = 0;
+      }
+      else {
+        data.minute++;
+      }
+      break;
+    case 1:
+      if (12 <= data.hour) {
+        data.hour = 0;
+      }
+      else {
+        data.hour++;
+      }
+      break;
+    case 0:
+      if (30 < data.day) {
+        data.day = 0;
+      }
+      else {
+        data.day++;
+      }
+      break;
+
+    default:
+      break;
+    }
+    makeSprite();
+  }
+
+  if (!mfrc522.PICC_IsNewCardPresent() ||
+      !mfrc522.PICC_ReadCardSerial()) { // 如果没有读取到新的卡片
+    delay(200);
+    return;
+  }
+  else {
+    data.uidSize = mfrc522.uid.size;
+    for (byte i = 0; i < mfrc522.uid.size; i++) {
+      data.uidByte[i] = mfrc522.uid.uidByte[i];
+    }
+    makeSprite();
+  }
+
+  delay(1);
+}
