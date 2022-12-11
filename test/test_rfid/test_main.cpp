30f154202bdbc37cbd6a30e2671ce72664ceef29
coppercele <mokeuhi@hotmail.com>
2022-12-11 18:31:02 +0900

    modified:   test/test_rfid/test_main.cpp

--- test/test_rfid/test_main.cpp
+++ test/test_rfid/test_main.cpp
@@ -1,12 +1,19 @@
 #include <Arduino.h>
 #include <unity.h>
-
-void setUp(void)
-{
+#include <SPI.h>
+#include <Wire.h>
+#include "FS.h"
+#include "SPIFFS.h"
+#include "SD.h"
+#include <HTTPClient.h>
+void setUp(void) {
   // set stuff up here
 }
 
-void tearDown(void)
-{
+void tearDown(void) {
+  // clean stuff up here
+}
+
+void loop() {
   // clean stuff up here
 }
